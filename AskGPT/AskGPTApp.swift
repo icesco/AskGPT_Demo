c2e9794b520bdee06cc39c9833836c3ef9431c14
Francesco Bianco <53116975+icesco@users.noreply.github.com>
2023-03-14 15:27:01 +0100

    Initial Commit

---
+++ AskGPT/AskGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  AskGPTApp.swift
+//  AskGPT
+//
+//  Created by Francesco Bianco on 14/03/23.
+//
+
+import SwiftUI
+
+@main
+struct AskGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AskGPT/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AskGPT
+//
+//  Created by Francesco Bianco on 14/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
